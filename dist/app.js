acf964da91aa6155839f6780bc32f8ef27b8eed5
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2020-12-25 20:15:27 +0300

    The rebuild process has been performed in the project for changes.

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,3 @@
-"use strict";const getData = require('./lib/service');
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _service = require('./lib/service'); var _service2 = _interopRequireDefault(_service);
 
-getData(3);
+_service2.default.call(void 0, 3);
